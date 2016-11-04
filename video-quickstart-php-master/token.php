b8ee69505a176ba56c861d21d19ad52ea84da515
kumar31 <kumarappan@smaatapps.com>
2016-11-04 18:00:58 +0530

    Update token.php

--- video-quickstart-php-master/token.php
+++ video-quickstart-php-master/token.php
@@ -14,13 +14,13 @@ $appName = 'TwilioVideoDemo';
 $identity = 'test-user';
 
 // Create access token, which we will serialize and send to the client
-/*$token = new AccessToken(
+$token = new AccessToken(
     $TWILIO_ACCOUNT_SID, 
     $TWILIO_API_KEY, 
     $TWILIO_API_SECRET, 
     3600, 
     $identity
-);*/
+);
 //$token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2YwZjM5YTU3ODJjZjg3NmNhMDAwNzY4ZmU5NTIzMjcxLTE0NzgyNjIyNTYiLCJpc3MiOiJTS2YwZjM5YTU3ODJjZjg3NmNhMDAwNzY4ZmU5NTIzMjcxIiwic3ViIjoiQUM1NGMyNzRmMjlhNTA2YzY1MTUxY2MyOWM3MTQ0ODY3MSIsImV4cCI6MTQ3ODI2NTg1NiwiZ3JhbnRzIjp7ImlkZW50aXR5IjoidGVzdC11c2VyIiwicnRjIjp7ImNvbmZpZ3VyYXRpb25fcHJvZmlsZV9zaWQiOiJWU2FhMWViOTRjM2Q1MmQ3ODIzYzIzYTFjZjBhOGQyNjk4In19fQ.yT6oh4AEclPNBnorO6xGGJyEBRsGWatCmI6ed9sTGU8";
 
 //echo $identity;
